9cf663ab949d073abe407e2d6aa34b1120edbde9
aaldossary <35977760+aaldossary@users.noreply.github.com>
2018-03-12 15:56:59 -0500

    Update printAll.js

--- js/printAll.js
+++ js/printAll.js
@@ -1,5 +1,10 @@
-var all = document.getElementsByTagName("*");
+/*var all = document.getElementsByTagName("*");
 
 for (var i=0, max=all.length; i < max; i++) {
      // Do something with the element here
 }
+*/
+
+
+var list = document.getElementsByClassName("page-heading")[0];
+list.getElementsByClassName("child")[0].innerHTML = "Milk";
