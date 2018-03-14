bd58d7f586a66fac959c1a173f14985041e6cfea
aaldossary <35977760+aaldossary@users.noreply.github.com>
2018-03-14 12:18:28 -0500

    Update printAll.js

--- js/printAll.js
+++ js/printAll.js
@@ -9,18 +9,20 @@ for (var i=0, max=all.length; i < max; i++) {
 var list = document.getElementsByClassName("page-heading");
 list.getElementsByClassName("post-list").innerHTML= "HI there";
 
-
+*/
 
 console.log("hi"); 
 
 var all = document.getElementsByTagName("*");
 
 for (var i=0, max=all.length; i < max; i++) {
-     // Do something with the element here
-	console.log(i); //all is an array...i is the array index...
+    
+	console.log(all); //all is an array...i is the array index...
 }
-*/
+
+/*
 var children = element.childNodes;
 children.forEach(function(item){
     console.log(item);
 });
+*/
