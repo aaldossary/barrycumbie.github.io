9391e2cb31567428e542d755448bb13fbe0605d2
aaldossary <35977760+aaldossary@users.noreply.github.com>
2018-03-14 12:20:44 -0500

    lastUpdated

--- js/printAll.js
+++ js/printAll.js
@@ -19,10 +19,3 @@ for (var i=0, max=all.length; i < max; i++) {
     
 	console.log(all); //all is an array...i is the array index...
 }
-
-/*
-var children = element.childNodes;
-children.forEach(function(item){
-    console.log(item);
-});
-*/
